edf905903105362f037cdbec2c3e115709bfe14f
iskandarthegre4t@gmail.com <iskandarthegre4t@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-03-26 11:11:39 +0000

    kurang menambahkan state pattern phase  pada objectloader  ada kemungkinan mengubah nama object loader menjadi phase manager

---
+++ modul-pertarungan/Assets/Deck.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+namespace ModulPertarungan
+{
+    public class Deck
+    {
+
+        // Use this for initialization
+        private List<GameObject> card;
+        public List<GameObject> Card
+        {
+            get { return card; }
+            set { card = value; }
+        }
+
+        public void Shuffle()
+        {
+            for (int i = 0; i < card.Count; i++)
+            {
+                GameObject tempCard = card[Random.Range(0, card.Count)];
+                card.Remove(tempCard);
+                card.Insert(Random.Range(0, card.Count), tempCard);
+            }
+        }
+        public GameObject Draw()
+        {
+            GameObject drawedcard = card[0];
+            card.RemoveAt(0);
+            return drawedcard;
+        }
+        public Deck(List<GameObject> card)
+        {
+            this.card = card;
+        }
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
