9580748e6edd7f204bbf6032659b4d7098a19291
iskandarthegre4t@gmail.com <iskandarthegre4t@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-04-24 06:25:38 +0000

    menambahkan missing asset

---
+++ modul-pertarungan/Assets/script/Factory/CardFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+namespace ModulPertarungan
+{
+	public class CardFactory:AbstractFactory
+	{
+        Dictionary<String, GameObject> ListCard;
+        GameObject card;
+        public override void InstantiateObject()
+        {
+            ListCard = new Dictionary<string, GameObject>();
+            foreach (GameObject obj in GameManager.Instance().AllCards)
+            {
+                ListCard.Add(obj.GetComponent<CardsEffect>().name, obj);
+            }
+        }
+        public override void CreateObject(string Objectname)
+        {
+            ListCard.TryGetValue(Objectname, out card);
+        }
+	}
+}
