bb5a346593076d4bac7411add61494fdf38d89fc
iskandarthegre4t@gmail.com <iskandarthegre4t@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-04-24 06:25:38 +0000

    menambahkan missing asset

---
+++ trunk/modul-pertarungan/Assets/script/Factory/CardEffectFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModulPertarungan
+{
+	public class CardEffecFactory:AbstractFactory
+	{
+        private Dictionary<string, CardsEffect> CreateCardList;
+        private CardsEffect card;
+        
+        public override void InstantiateObject()
+        {
+            CreateCardList = new Dictionary<string, CardsEffect>();
+            card= new FireStorm();
+            CreateCardList.Add("FireStormCard",card);
+            card = new SplitFireCard();
+            CreateCardList.Add("SplitFireCard", card);
+            card = new TyphoonCard();
+            CreateCardList.Add("TyphoonCard", card);
+            card = new WindStorm();
+            CreateCardList.Add("WindStromCard", card);
+            card = new TidalWaveCard();
+            CreateCardList.Add("TidalWaveCard", card);
+            card = new WaterAttackAction();
+            CreateCardList.Add("WaterAttack", card);    
+        }
+        
+        public override void CreateObject(string Objectname)
+        {
+            CreateCardList.TryGetValue(Objectname, out card);
+            card.Effect();
+        }
+	}
+}
