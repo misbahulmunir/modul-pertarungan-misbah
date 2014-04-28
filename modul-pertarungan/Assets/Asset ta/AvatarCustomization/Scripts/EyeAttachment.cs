bd13eea6c04d2464a524f9ca99102e283171c272
zendraanugerah@gmail.com <zendraanugerah@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-04-28 06:15:51 +0000

    tambah modul AvatarCostumization

---
+++ modul-pertarungan/Assets/Asset ta/AvatarCustomization/Scripts/EyeAttachment.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeAttachment : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		OnClick();
+	}
+	
+	public GameObject eyeStyle1, eyeStyle2, eyeStyle3;
+	Object eChild;
+	
+	public void AttachEye (string styleChosen)
+	{
+		Destroy (eChild);
+		var currentPos = this.transform.position;
+		if (styleChosen == "menumata1") 
+		{
+			var eyeStyle =  Instantiate(eyeStyle1, currentPos, Quaternion.identity);
+			eChild = eyeStyle;
+			Debug.Log("eyestyle1");
+		} else
+			if (styleChosen == "menumata2") 
+		{
+			var eyeStyle =  Instantiate(eyeStyle2, currentPos, Quaternion.identity);
+			eChild = eyeStyle;
+			Debug.Log("eyestyle2");
+		} else
+			if (styleChosen == "menumata3") 
+		{
+			var eyeStyle =  Instantiate(eyeStyle3, currentPos, Quaternion.identity);
+			eChild = eyeStyle;
+			Debug.Log("eyestyle3");
+		}
+	}
+	
+	void OnClick()
+	{
+		RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+		if (Input.GetMouseButtonUp(0))
+		{
+			if (hit.collider != null)
+			{
+				if (hit.collider.gameObject.name.ToLower().Contains("menumata"))
+				{
+					GameObject objE = hit.collider.gameObject as GameObject;
+					Debug.Log(objE.name);
+					AttachEye(objE.name);
+				}
+			}
+		}
+	}
+}
---
+++ modul-pertarungan/Assets/Asset ta/AvatarCustomization/Scripts/MenuClick.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MenuClick : MonoBehaviour {
+	
+	public GameObject hairList;
+	public GameObject mouthList;
+	public GameObject eyeList;
+	int state = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		OnClick ();
+	}
+
+	void OnClick()
+	{
+		RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+		if (Input.GetMouseButtonUp(0))
+		{
+			if (hit.collider != null)
+			{
+				if (hit.collider.gameObject.name.ToLower().Contains("submenurambut") && state != 0)
+				{
+					GameObject obj = hit.collider.gameObject as GameObject;
+					Debug.Log(obj.name);
+					if(mouthList.transform.position.z == -2) mouthList.transform.Translate(0,0,4);
+					if(eyeList.transform.position.z == -2) eyeList.transform.Translate(0,0,4);
+					hairList.transform.Translate(0,0,-4);
+					state = 0;
+				} else
+				if (hit.collider.gameObject.name.ToLower().Contains("submenumata") && state != 1)
+				{
+					GameObject obj = hit.collider.gameObject as GameObject;
+					Debug.Log(obj.name);
+					if(mouthList.transform.position.z == -2) mouthList.transform.Translate(0,0,4);
+					if(hairList.transform.position.z == -2) hairList.transform.Translate(0,0,4);
+					eyeList.transform.Translate(0,0,-4);
+					state = 1;
+				} else
+				if (hit.collider.gameObject.name.ToLower().Contains("submenumulut") && state != 2)
+				{
+					GameObject obj = hit.collider.gameObject as GameObject;
+					Debug.Log(obj.name);
+					if(hairList.transform.position.z == -2) hairList.transform.Translate(0,0,4);
+					if(eyeList.transform.position.z == -2) eyeList.transform.Translate(0,0,4);
+					mouthList.transform.Translate(0,0,-4);
+					state = 2;
+				}
+			}
+		}
+	}
+}
