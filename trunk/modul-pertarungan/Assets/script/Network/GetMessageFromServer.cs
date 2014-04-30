8017f2695d8518f01ce74d7ff015e708d2cfdda4
iskandarthegre4t@gmail.com <iskandarthegre4t@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-04-30 08:40:06 +0000

    mengubah BattleStateManager menjadi lebih ringkas dengan fungsi HitCollider

---
+++ trunk/modul-pertarungan/Assets/script/Network/GetMessageFromServer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+namespace ModulPertarungan
+{
+	public class GetMessageFromServer:MonoBehaviour
+	{
+        public GameObject label;
+        void Update()
+        {
+            
+        }
+
+        void Start()
+        {
+
+        }
+
+        public void ReceiveMessage(string Message)
+        {
+            label.GetComponent<UILabel>().text = Message;
+        }
+	}
+}
---
+++ trunk/modul-pertarungan/Assets/script/State/PvpEnemyState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+namespace ModulPertarungan
+{
+    public class PvpEnemyState : BattleState
+    {
+        public PvpEnemyState(List<GameObject> Players, List<GameObject> Enemy, BattleStateManager BattleManager)
+            : base(Players, Enemy, BattleManager)
+        {
+
+        }
+        public override void Action()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
