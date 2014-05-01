864762489a4447b66cf6be76b4d43c4d23eef144
iskandarthegre4t@gmail.com <iskandarthegre4t@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-05-01 09:12:05 +0000

    menambahkan fitur SearchCard.cs pada DeckEditor.unity

---
+++ modul-pertarungan/Assets/SearchCard.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SearchCard : MonoBehaviour
+{
+
+    // Use this for initialization
+    public GameObject grid;
+    public GameObject searchText;
+    private string cardName;
+    void OnClick()
+    {
+        RefreshCard();
+        foreach (Transform trans in grid.transform)
+        {
+            if (!(trans.gameObject.name.ToLower().Contains(cardName.ToLower())))
+            {
+                trans.gameObject.SetActive(false);
+            }
+        }
+
+        grid.GetComponent<UIGrid>().Reposition();
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cardName = searchText.GetComponent<UILabel>().text;
+    }
+    public void RefreshCard()
+    {
+        foreach (Transform trans in grid.transform)
+        {
+            trans.gameObject.SetActive(true);
+        }
+    }
+}
