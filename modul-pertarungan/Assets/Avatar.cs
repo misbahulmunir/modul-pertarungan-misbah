c56d612d989a80c90173f1505caedb0fbea018ec
iskandarthegre4t@gmail.com <iskandarthegre4t@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-05-05 04:09:24 +0000

    menambahkan PlayerEditorManager.cs

---
+++ modul-pertarungan/Assets/Avatar.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Avatar : MonoBehaviour {
+
+	// Use this for initialization
+
+
+    private string name;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    private string level;
+
+    public string Level
+    {
+        get { return level; }
+        set { level = value; }
+    }
+
+    private string job;
+
+    public string Job
+    {
+        get { return job; }
+        set { job = value; }
+    }
+
+    private string rank;
+
+    public string Rank
+    {
+        get { return rank; }
+        set { rank = value; }
+    }
+	void Start () {
+        this.name = "UUL";
+        foreach (Transform t in this.transform)
+        {
+            if (t.gameObject.name == "Name")
+            {
+                t.GetComponent<UILabel>().text = this.name;
+            }
+            else if (t.gameObject.name == "Job")
+            {
+                t.GetComponent<UILabel>().text = this.job;
+            }
+            else if (t.gameObject.name == "Rank")
+            {
+                t.GetComponent<UILabel>().text = this.Rank;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ modul-pertarungan/Assets/script/Manager/PlayerEditorManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+namespace ModulPertarungan
+{
+	public class PlayerEditorManager:MonoBehaviour
+	{
+        public GameObject grid;
+        public GameObject avatar;
+        void Update()
+        {
+           
+        }
+
+
+        void Start()
+        {
+            for (int c = 0; c < 2; c++)
+            {
+                NGUITools.AddChild(grid,avatar);
+            }
+        }
+	}
+}
