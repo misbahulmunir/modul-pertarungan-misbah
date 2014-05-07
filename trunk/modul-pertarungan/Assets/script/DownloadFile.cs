e014d20ae702656b56164683af3ed055c2959ea9
wesker9110@gmail.com <wesker9110@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-05-07 08:37:56 +0000

    Load Deck Kartu dari Web Service (in progress)

---
+++ trunk/modul-pertarungan/Assets/script/DownloadFile.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.ComponentModel;
+using System;
+
+public class DownloadFile : MonoBehaviour {
+
+    int progress;
+	// Use this for initialization
+	void Start () {
+        progress = 0;
+        DownloadXMLFile();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Loading : " + progress + "% complete");
+	}
+
+    private void DownloadXMLFile()
+    {
+      string path = Application.dataPath + "/myfile.xml";
+      WebClient webClient = new WebClient();
+      webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+      webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+      webClient.DownloadFileAsync(new Uri("http://cws.yowanda.com/files/player_profile_agil.xml"), path);
+    }
+
+    private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+    {
+      progress = e.ProgressPercentage;
+    }
+
+    private void Completed(object sender, AsyncCompletedEventArgs e)
+    {
+      
+    }
+
+}
