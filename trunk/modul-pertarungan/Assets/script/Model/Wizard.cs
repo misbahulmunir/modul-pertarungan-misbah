b529160ca05ddf74f9991268ae2b608049ed7e89
iskandarthegre4t@gmail.com <iskandarthegre4t@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-05-20 07:47:36 +0000

    refactor

---
+++ trunk/modul-pertarungan/Assets/script/Model/Wizard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Wizard : ModelModulPertarungan.Player
+{
+    public Wizard(int MaxHealth, int CurrentHealth, string Name, int HandCapacity)
+        : base(MaxHealth, CurrentHealth, Name, HandCapacity)
+    {
+    }
+
+    public Wizard()
+    {
+
+    }
+}
