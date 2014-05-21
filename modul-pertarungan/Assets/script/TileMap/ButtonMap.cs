7dc4df8278ab67b6ff3a5dfcd59a001775ebd282
rizal.maulidan@gmail.com <rizal.maulidan@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-05-21 16:10:26 +0000

    nambahi ContinentDungeon

---
+++ modul-pertarungan/Assets/script/TileMap/ButtonMap.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonMap : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnClick();
+    }
+    void OnClick()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.name.ToLower().Contains("dungeonbutton"))
+                {
+                    Application.LoadLevel("MapDungeon");
+                }
+            }
+        }
+    }
+}
