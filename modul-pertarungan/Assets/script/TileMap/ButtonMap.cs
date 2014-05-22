b22ad2e1a46d4985e90a991314fa7f643ef65dbe
rizal.maulidan@gmail.com <rizal.maulidan@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-05-22 09:53:13 +0000

    benerin button :3

--- modul-pertarungan/Assets/script/TileMap/ButtonMap.cs
+++ modul-pertarungan/Assets/script/TileMap/ButtonMap.cs
@@ -1,31 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ButtonMap : MonoBehaviour
+public class ButtonMap : Button
 {
     // Use this for initialization
     void Start()
     {
-
+        SceneLoad = "MapDungeon";
+        ButtonName = "mapbutton";
     }
 
     // Update is called once per frame
     void Update()
     {
-        OnClick();
+       
     }
-    void OnClick()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-        if (Input.GetMouseButtonUp(0))
-        {
-            if (hit.collider != null)
-            {
-                if (hit.collider.gameObject.name.ToLower().Contains("dungeonbutton"))
-                {
-                    Application.LoadLevel("MapDungeon");
-                }
-            }
-        }
-    }
+    
 }
