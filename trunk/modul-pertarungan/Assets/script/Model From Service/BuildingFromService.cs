d30c2b97a48e6db73a6032cc0b69639923fb07b7
wesker9110@gmail.com <wesker9110@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-05-23 13:43:22 +0000

    Create instance object dari service (membantu zendra)

---
+++ trunk/modul-pertarungan/Assets/script/Model From Service/BuildingFromService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModulPertarungan
+{
+	public class BuildingFromService
+	{
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int MaxXP { get; set; }
+        public int CurrentXP { get; set; }
+        public int ProductionQuantity { get; set; }
+        public int ProductClaimed { get; set; }
+        public string PrefabName { get; set; }
+	}
+}
