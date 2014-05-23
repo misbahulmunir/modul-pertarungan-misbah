11c3b902f6c553b0bce21d3593be2c5130aa8828
rizal.maulidan@gmail.com <rizal.maulidan@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-05-23 15:09:58 +0000

    nambah folder buat masing elemen di model sama actionscript

---
+++ modul-pertarungan/Assets/script/Model/Earth/EarthMonster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModelModulPertarungan
+{
+    public class EarthMonster : Enemy
+    {
+        public EarthMonster(int MaxHealth, int CurrentHealth, string Name)
+            : base(MaxHealth, CurrentHealth, Name)
+        {
+        }
+
+        public EarthMonster()
+        {
+
+        }
+    }
+}
