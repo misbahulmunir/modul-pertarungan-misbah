23cc54ea6627150cd288de318bbb1ed0331bd4f1
wesker9110@gmail.com <wesker9110@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-05-26 20:53:47 +0000

    Update Show Player Ranking

---
+++ modul-pertarungan/Assets/script/Model From Service/Player Ranking/PlayerStatsFromService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModulPertarungan
+{
+	public class PlayerStatsFromService
+	{
+        public string Name { get; set; }
+        public int BattleWon { get; set; }
+	}
+}
