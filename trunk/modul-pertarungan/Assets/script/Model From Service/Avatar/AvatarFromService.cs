7ae949a04f90e296e260a3b51759c2eac46f9a0b
wesker9110@gmail.com <wesker9110@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-05-27 10:11:58 +0000

    Refactor dikit

---
+++ trunk/modul-pertarungan/Assets/script/Model From Service/Avatar/AvatarFromService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ModulPertarungan
+{
+	public class AvatarFromService
+	{
+        [XmlElement("AviFromService")]
+        public List<AviFromService> aviDetail = new List<AviFromService>();
+	}
+}
---
+++ trunk/modul-pertarungan/Assets/script/Model From Service/Avatar/AviFromService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModulPertarungan
+{
+	public class AviFromService
+	{
+        public string Slot { get; set; }
+        public string Name { get; set; }
+	}
+}
