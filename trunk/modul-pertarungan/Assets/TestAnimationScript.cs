7d45d707adc38d383bf049db9107f6659a5b2468
iskandarthegre4t@gmail.com <iskandarthegre4t@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-06-05 09:06:02 +0000

    menambahkan animasi rank a dan rank c

---
+++ trunk/modul-pertarungan/Assets/TestAnimationScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAnimationScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Animator>().SetBool(
+          "IsAttack", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+}
