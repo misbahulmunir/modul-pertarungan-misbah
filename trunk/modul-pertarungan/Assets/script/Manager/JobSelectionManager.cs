bbb351ed618f5d831d1ef7da8626862a93e8405f
iskandarthegre4t@gmail.com <iskandarthegre4t@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-06-06 23:33:31 +0000

    menambakan JobSelection.unity

--- trunk/modul-pertarungan/Assets/script/Manager/JobSelectionManager.cs
+++ trunk/modul-pertarungan/Assets/script/Manager/JobSelectionManager.cs
@@ -1,45 +1,72 @@
-﻿using  UnityEngine;
+﻿using Holoville.HOTween;
+using UnityEngine;
 
 namespace ModulPertarungan
 {
-	public class JobSelectionManager:MonoBehaviour
-	{
-	    public UILabel jobName;
-	    public UILabel description;
-	    public UITexture characterImage;
-	    void Start()
-	    {
-	        
-	    }
+    public class JobSelectionManager : MonoBehaviour
+    {
+        public UILabel jobName;
+        public UILabel description;
+        public UITexture characterImagePanel;
+        public Texture characterImage;
+        public GameObject picturePanelPosition;
+        public GameObject descriptionPanePosition;
+        public GameObject picturePanelPositionStart;
+        public GameObject descriptionPanePositionStart;
+        public GameObject picturePanel;
+        public GameObject descriptionPanel;
+        void Start()
+        {
 
-	    private void Update()
-	    {
+        }
 
-	    }
+        private void Update()
+        {
 
-	    public void SelectMagician()
-	    {
-	        
-	    }
+        }
 
-	    public void SelectWarlock()
-	    {
+        public void SelectMagician()
+        {  TweenObjectOut();
+           characterImagePanel.mainTexture = characterImage;
+           TweenObjectIn();
+        }
 
-	    }
+        public void SelectWarlock()
+        {
+            TweenObjectOut();
+            characterImagePanel.mainTexture = characterImage;
+            TweenObjectIn();
+        }
 
-	    public void SelectWizard()
-	    {
+        public void SelectWizard()
+        {
 
-	    }
+        }
 
-	    public void SelectSorcerer()
-	    {
-	        
-	    }
+        public void SelectSorcerer()
+        {
 
-	    public void SelectGrandMagus()
-	    {
-	        
-	    }
-	}
+        }
+
+        public void SelectGrandMagus()
+        {
+
+        }
+
+        public void TweenObjectIn()
+        {
+            var parms= new TweenParms();
+            parms.Prop("position", picturePanelPosition.transform.position);
+            HOTween.To(picturePanel.transform, 1f, parms);
+            parms = new TweenParms();
+            parms.Prop("position", descriptionPanePosition.transform.position);
+            HOTween.To(descriptionPanel.transform, 1f, parms);
+        }
+
+        public void TweenObjectOut()
+        {
+            descriptionPanel.transform.position = descriptionPanePositionStart.transform.position;
+            picturePanel.transform.position = picturePanelPositionStart.transform.position;
+        }
+    }
 }
