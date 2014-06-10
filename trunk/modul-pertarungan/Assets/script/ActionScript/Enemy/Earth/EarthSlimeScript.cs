66f73031d7cefd50ef7516746c1180b6ea8c3fff
iskandarthegre4t@gmail.com <iskandarthegre4t@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-06-10 18:01:30 +0000

    menambahkan earthmonster

--- trunk/modul-pertarungan/Assets/script/ActionScript/Enemy/Earth/EarthSlimeScript.cs
+++ trunk/modul-pertarungan/Assets/script/ActionScript/Enemy/Earth/EarthSlimeScript.cs
@@ -36,13 +36,8 @@ namespace ModulPertarungan
         }
         public override void ReceiveDamage(DamageReceiver damageReceiver,CardsEffect damageGiver,int damage)
         {
+            this.Enemy = earthslime;
             base.ReceiveDamage(damageReceiver, damageGiver, damage);
-            if (this.earthslime.CurrentHealth <= 0)
-            {
-                Destroy(this.gameObject);
-            }
-
-            Debug.Log(GameManager.Instance().Enemies.Count);
         }
     }
 }
--- trunk/modul-pertarungan/Assets/script/ActionScript/Enemy/Earth/MandrakeScript.cs
+++ trunk/modul-pertarungan/Assets/script/ActionScript/Enemy/Earth/MandrakeScript.cs
@@ -36,13 +36,8 @@ namespace ModulPertarungan
         }
         public override void ReceiveDamage(DamageReceiver damageReceiver, CardsEffect damageGiver, int damage)
         {
+            this.Enemy = mandrake;
             base.ReceiveDamage(damageReceiver, damageGiver, damage);
-            if (this.mandrake.CurrentHealth <= 0)
-            {
-                Destroy(this.gameObject);
-            }
-
-            Debug.Log(GameManager.Instance().Enemies.Count);
         }
     }
 }
