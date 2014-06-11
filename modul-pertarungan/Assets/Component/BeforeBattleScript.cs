6df379766a58b0e189909c9e4d5b7ff3f5e7fc3f
iskandarthegre4t@gmail.com <iskandarthegre4t@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-06-11 00:28:12 +0000

    refactor BeforeBattleScript

--- modul-pertarungan/Assets/Component/BeforeBattleScript.cs
+++ modul-pertarungan/Assets/Component/BeforeBattleScript.cs
@@ -16,23 +16,23 @@ namespace ModulPertarungan
 
         void OnClick()
         {
-            try
-            {
-                XmlSerializer deserializer = new XmlSerializer(typeof(FriendListFromService));
-                TextReader textReader = new StreamReader(Application.persistentDataPath + "/friends_of_" + GameManager.Instance().PlayerId + ".xml");
-                object obj = deserializer.Deserialize(textReader);
-                var players = (FriendListFromService)obj;
-                foreach (var s in players.players)
-                {
-                    Debug.Log(s.Name);
-                    Debug.Log(s.Job);
-                }
-                textReader.Close();
-            }
-            catch (Exception e)
-            {
-                Debug.Log(e);
-            }
+            //try
+            //{
+            //    XmlSerializer deserializer = new XmlSerializer(typeof(FriendListFromService));
+            //    TextReader textReader = new StreamReader(Application.persistentDataPath + "/friends_of_" + GameManager.Instance().PlayerId + ".xml");
+            //    object obj = deserializer.Deserialize(textReader);
+            //    var players = (FriendListFromService)obj;
+            //    foreach (var s in players.players)
+            //    {
+            //        Debug.Log(s.Name);
+            //        Debug.Log(s.Job);
+            //    }
+            //    textReader.Close();
+            //}
+            //catch (Exception e)
+            //{
+            //    Debug.Log(e);
+            //}
               //Application.LoadLevel("Battle");
 
             questActived = new bool[] { true, false, false, false, false, false, false, false };
