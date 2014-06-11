f656fd52dbb48c3ae21ad7f59f211e47bae61276
wesker9110@gmail.com <wesker9110@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-06-11 16:00:32 +0000

    Penambahan Model Load Data XML dari Service untuk Tabel Quest dan Monster

---
+++ trunk/modul-pertarungan/Assets/script/Model From Service/Monster_and_Quest/BattleListFromService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModulPertarungan
+{
+	public class BattleListFromService
+	{
+        public string ID { get; set; }
+	}
+}
---
+++ trunk/modul-pertarungan/Assets/script/Model From Service/Monster_and_Quest/QuestFromService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModulPertarungan
+{
+	public class QuestFromService
+	{
+        public string ID { get; set; }
+        public bool IsClear { get; set; }
+        public bool IsActive { get; set; }
+	}
+}
