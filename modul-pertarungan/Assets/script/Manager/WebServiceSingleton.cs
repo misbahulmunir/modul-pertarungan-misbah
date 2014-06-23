fcdf503ab76b2056b323cb37bcfbd6e837e4302b
wesker9110@gmail.com <wesker9110@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-06-23 15:48:00 +0000

    Tambah satu line of code pada url web service singleton

--- modul-pertarungan/Assets/script/Manager/WebServiceSingleton.cs
+++ modul-pertarungan/Assets/script/Manager/WebServiceSingleton.cs
@@ -152,6 +152,7 @@ namespace ModulPertarungan
             urlDictionary.Add("get_battle_list", "http://cws.yowanda.com/ClientController/1/monster_quest/get_battle_list");
             urlDictionary.Add("get_player_quest", "http://cws.yowanda.com/ClientController/1/monster_quest/get_player_quest");
             urlDictionary.Add("get_name_by_fb", "http://cws.yowanda.com/ClientController/1/player/get_name_by_fb");
+			urlDictionary.Add("send_battle_result", "http://cws.yowanda.com/ClientController/2/battle/send_battle_result");
         }
 
         private void InitPathDictionary()
