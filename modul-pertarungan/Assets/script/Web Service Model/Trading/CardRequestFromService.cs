b2f08bd28eadddd71b07a080adf6e42244d1de61
wesker9110@gmail.com <wesker9110@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-06-24 08:03:09 +0000

    Trade Request Handling

---
+++ modul-pertarungan/Assets/script/Web Service Model/Trading/CardRequestFromService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ModulPertarungan
+{
+	public class CardRequestFromService
+	{
+        [XmlElement("OfferedCardsFromService")]
+        public List<CardRequest> offeredCards = new List<CardRequest>();
+
+        [XmlElement("RequestedCardsFromService")]
+        public List<CardRequest> requestedCards = new List<CardRequest>(); 
+	}
+}
