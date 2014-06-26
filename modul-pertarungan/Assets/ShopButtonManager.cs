84386e1d102d206e0230f296f630faaf620ceb08
iskandarthegre4t@gmail.com <iskandarthegre4t@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-06-26 02:38:10 +0000

    menabahkan Shop.prefab

---
+++ modul-pertarungan/Assets/ShopButtonManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using ModulPertarungan;
+using Holoville.HOTween;
+public class ShopButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+    Vector3 startPosisiton;
+	void Start () {
+        startPosisiton = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void ShowShop()
+    {
+        var parms = new TweenParms();
+        parms.Prop("position", new Vector3(0f,0.5f,0f));
+        HOTween.To(this.transform, 1f, parms);
+    }
+    void CloseShop()
+    {
+        var parms = new TweenParms();
+        parms.Prop("position", startPosisiton);
+        HOTween.To(this.transform, 1f, parms);
+    }
+}
