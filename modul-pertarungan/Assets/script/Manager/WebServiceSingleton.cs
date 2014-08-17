15f99f54ea92bb89373976b98c454057ef6dd5a9
wesker9110@gmail.com <wesker9110@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-08-17 19:58:58 +0000

    Habis migrasi

--- modul-pertarungan/Assets/script/Manager/WebServiceSingleton.cs
+++ modul-pertarungan/Assets/script/Manager/WebServiceSingleton.cs
@@ -53,10 +53,20 @@ namespace ModulPertarungan
             {
                 responseFromServer = "-3|Error reading stream";
             }
-            Debug.Log(methodName + " " /*+ parameter */+ responseFromServer);
-            string[] response = responseFromServer.Split('|');
-            queryResult = int.Parse(response[0]);
-            queryInfo = response[1];
+            //Debug.Log(methodName + " " /*+ parameter */+ responseFromServer);
+
+            try
+            {
+                string[] response = responseFromServer.Split('|');
+                queryResult = int.Parse(response[0]);
+                queryInfo = response[1];
+            }
+            catch
+            {
+                queryResult = 2;
+                queryInfo = responseFromServer;
+            }
+            Debug.Log("query result : " + queryInfo);
         }
 
         public string DownloadFile(string methodName, string parameter)
@@ -130,44 +140,44 @@ namespace ModulPertarungan
         private void InitUrlDictionary()
         {
             urlDictionary = new Dictionary<string, string>();
-            urlDictionary.Add("get_profile", "http://cws.yowanda.com/ClientController/1/player/get_profile");
-            urlDictionary.Add("get_friend_list", "http://cws.yowanda.com/ClientController/1/player/get_friend_list");
-            urlDictionary.Add("get_player_deck", "http://cws.yowanda.com/ClientController/2/card/get_cards/deck");
-            urlDictionary.Add("get_player_trunk", "http://cws.yowanda.com/ClientController/2/card/get_cards/trunk");
-            urlDictionary.Add("clear_deck", "http://cws.yowanda.com/ClientController/1/card/clear_deck");
-            urlDictionary.Add("insert_to_deck", "http://cws.yowanda.com/ClientController/3/card/insert_to_deck");
-            urlDictionary.Add("get_list_avatar", "http://cws.yowanda.com/ClientController/2/avatar/get_avatar/list");
-            urlDictionary.Add("get_player_avatar", "http://cws.yowanda.com/ClientController/2/avatar/get_avatar/player");
-            urlDictionary.Add("get_building", "http://cws.yowanda.com/ClientController/1/building/get_building");
-            urlDictionary.Add("get_battle_rank", "http://cws.yowanda.com/ClientController/1/battle/show_battle_rank");
-            urlDictionary.Add("get_player_ranking", "http://cws.yowanda.com/ClientController/0/battle/show_player_ranking");
-            urlDictionary.Add("get_friend_request", "http://cws.yowanda.com/ClientController/2/request/get_request_list/friend");
-            urlDictionary.Add("get_partial_profile", "http://cws.yowanda.com/ClientController/1/player/get_partial_profile");
-            urlDictionary.Add("send_friend_request", "http://cws.yowanda.com/ClientController/3/request/do_request/send-friend");
-            urlDictionary.Add("accept_friend_request", "http://cws.yowanda.com/ClientController/3/request/do_request/accept-friend");
-            urlDictionary.Add("edit_avatar", "http://cws.yowanda.com/ClientController/2/avatar/edit_avatar");
-            urlDictionary.Add("update_building", "http://cws.yowanda.com/ClientController/3/building/update_building");
-            urlDictionary.Add("ignore_friend_request", "http://cws.yowanda.com/ClientController/3/request/do_request/ignore-friend");
-            urlDictionary.Add("remove_friend", "http://cws.yowanda.com/ClientController/2/player/remove_friend");
-            urlDictionary.Add("register", "http://cws.yowanda.com/ClientController/3/player/register");
-            urlDictionary.Add("send_message", "http://cws.yowanda.com/ClientController/3/message/send_message");
-            urlDictionary.Add("get_messages", "http://cws.yowanda.com/ClientController/1/message/get_messages");
-            urlDictionary.Add("get_monster_list", "http://cws.yowanda.com/ClientController/1/monster_quest/get_monster_list");
-            urlDictionary.Add("get_battle_list", "http://cws.yowanda.com/ClientController/1/monster_quest/get_battle_list");
-            urlDictionary.Add("get_player_quest", "http://cws.yowanda.com/ClientController/1/monster_quest/get_player_quest");
-            urlDictionary.Add("get_name_by_fb", "http://cws.yowanda.com/ClientController/1/player/get_name_by_fb");
-			urlDictionary.Add("send_battle_result", "http://cws.yowanda.com/ClientController/2/battle/send_battle_result");
-            urlDictionary.Add("send_trade_request", "http://cws.yowanda.com/ClientController/1/request/send_trade_request");
-            urlDictionary.Add("get_trade_request_list", "http://cws.yowanda.com/ClientController/1/request/get_trade_request_list");
-            urlDictionary.Add("get_card_request_list", "http://cws.yowanda.com/ClientController/2/request/get_card_request_list");
-            urlDictionary.Add("decline_trade_request", "http://cws.yowanda.com/ClientController/1/request/decline_trade_request");
-            urlDictionary.Add("accept_trade_request", "http://cws.yowanda.com/ClientController/1/request/accept_trade_request");
-            urlDictionary.Add("login", "http://cws.yowanda.com/ClientController/2/player/login");
-            urlDictionary.Add("get_gift", "http://cws.yowanda.com/ClientController/2/request/get_gift");
-            urlDictionary.Add("share_gift", "http://cws.yowanda.com/ClientController/1/request/share_gift");
-            urlDictionary.Add("claim_gift", "http://cws.yowanda.com/ClientController/2/request/claim_gift");
-            urlDictionary.Add("get_gift_notif", "http://cws.yowanda.com/ClientController/1/request/get_gift_notif");
-            urlDictionary.Add("delete_message", "http://cws.yowanda.com/service/3/message/delete_message");
+            urlDictionary.Add("get_profile", "http://199.175.51.79:8001/get_profile");
+            urlDictionary.Add("get_friend_list", "http://199.175.51.79:8001/get_friend_list");
+            urlDictionary.Add("get_player_deck", "http://199.175.51.79:8001/get_player_deck");
+            urlDictionary.Add("get_player_trunk", "http://199.175.51.79:8001/get_player_trunk");
+            urlDictionary.Add("clear_deck", "http://199.175.51.79:8001/clear_deck");
+            urlDictionary.Add("insert_to_deck", "http://199.175.51.79:8001/insert_to_deck");
+            urlDictionary.Add("get_list_avatar", "http://199.175.51.79:8001/get_list_avatar");
+            urlDictionary.Add("get_player_avatar", "http://199.175.51.79:8001/get_player_avatar");
+            urlDictionary.Add("get_building", "http://199.175.51.79:8001/get_building");
+            urlDictionary.Add("get_battle_rank", "http://199.175.51.79:8001/show_battle_rank");
+            urlDictionary.Add("get_player_ranking", "http://199.175.51.79:8001/show_player_ranking");
+            urlDictionary.Add("get_friend_request", "http://199.175.51.79:8001/get_request_list/friend");
+            urlDictionary.Add("get_partial_profile", "http://199.175.51.79:8001/get_partial_profile");
+            urlDictionary.Add("send_friend_request", "http://199.175.51.79:8001/do_request/send-friend");
+            urlDictionary.Add("accept_friend_request", "http://199.175.51.79:8001/do_request/accept-friend");
+            urlDictionary.Add("edit_avatar", "http://199.175.51.79:8001/edit_avatar");
+            urlDictionary.Add("update_building", "http://199.175.51.79:8001/update_building");
+            urlDictionary.Add("ignore_friend_request", "http://199.175.51.79:8001/do_request/ignore-friend");
+            urlDictionary.Add("remove_friend", "http://199.175.51.79:8001/remove_friend");
+            urlDictionary.Add("register", "http://199.175.51.79:8001/register");
+            urlDictionary.Add("send_message", "http://199.175.51.79:8001/send_message");
+            urlDictionary.Add("get_messages", "http://199.175.51.79:8001/get_messages");
+            urlDictionary.Add("get_monster_list", "http://199.175.51.79:8001/get_monster_list");
+            urlDictionary.Add("get_battle_list", "http://199.175.51.79:8001/get_battle_list");
+            urlDictionary.Add("get_player_quest", "http://199.175.51.79:8001/get_player_quest");
+            urlDictionary.Add("get_name_by_fb", "http://199.175.51.79:8001/get_name_by_fb");
+            urlDictionary.Add("send_battle_result", "http://199.175.51.79:8001/send_battle_result");
+            urlDictionary.Add("send_trade_request", "http://199.175.51.79:8001/send_trade_request");
+            urlDictionary.Add("get_trade_request_list", "http://199.175.51.79:8001/get_trade_request_list");
+            urlDictionary.Add("get_card_request_list", "http://199.175.51.79:8001/get_card_request_list");
+            urlDictionary.Add("decline_trade_request", "http://199.175.51.79:8001/decline_trade_request");
+            urlDictionary.Add("accept_trade_request", "http://199.175.51.79:8001/accept_trade_request");
+            urlDictionary.Add("login", "http://199.175.51.79:8001/login");
+            urlDictionary.Add("get_gift", "http://199.175.51.79:8001/get_gift");
+            urlDictionary.Add("share_gift", "http://199.175.51.79:8001/share_gift");
+            urlDictionary.Add("claim_gift", "http://199.175.51.79:8001/claim_gift");
+            urlDictionary.Add("get_gift_notif", "http://199.175.51.79:8001/get_gift_notif");
+            urlDictionary.Add("delete_message", "http://199.175.51.79:8001/delete_message");
         }
 
         private void InitPathDictionary()
