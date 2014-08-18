3c3ae05e146c8ad299766141a3e91cdcddc4e37e
iskandarthegre4t@gmail.com <iskandarthegre4t@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-08-18 14:31:07 +0000

    Menambhakan ShopCard

---
+++ trunk/modul-pertarungan/Assets/ShopCardClick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopCardClick : MonoBehaviour {
+    public GameObject ConfirmationBoard;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnClick()
+    {
+        object obj = new object();
+        obj = this.gameObject.name;
+        ConfirmationBoard.SendMessage("ShowPurchase", obj);
+        Debug.Log("KLIK");
+    }
+}
