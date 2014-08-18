fbd50d62f7215bdc8d8c9f7875292172c2bef134
zendraanugerah@gmail.com <zendraanugerah@gmail.com@8f2a65a9-8ad1-cca0-9f54-064d0c22d208>
2014-08-18 17:14:07 +0000

    pesanan ichank

---
+++ modul-pertarungan/Assets/script/Web Service Model/Map_and_Dungeon/MapStatusFromService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ModulPertarungan
+{
+    public class MapStatusFromService
+    {
+        [XmlElement("MapStatus")]
+        public List<MapStatus> mapList = new List<MapStatus>();
+    }
+}
